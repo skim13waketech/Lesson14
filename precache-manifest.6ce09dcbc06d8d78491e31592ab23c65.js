603c44210599409641a30f66fb89db423c7c6efc
skim13waketech <56847449+skim13waketech@users.noreply.github.com>
2020-07-12 20:02:46 -0400

    Add files via upload

---
+++ precache-manifest.6ce09dcbc06d8d78491e31592ab23c65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92db85881e0362286b55c2d77050c545",
+    "url": "/Skim/WEB215/Lesson14/index.html"
+  },
+  {
+    "revision": "351b3f5d6760f78c8e30",
+    "url": "/Skim/WEB215/Lesson14/static/css/main.8cf58fc1.chunk.css"
+  },
+  {
+    "revision": "d73ea675c45b43a980ec",
+    "url": "/Skim/WEB215/Lesson14/static/js/2.5c20e2d1.chunk.js"
+  },
+  {
+    "revision": "3143274d7d26932c98894a52e75c487a",
+    "url": "/Skim/WEB215/Lesson14/static/js/2.5c20e2d1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "351b3f5d6760f78c8e30",
+    "url": "/Skim/WEB215/Lesson14/static/js/main.5b1e768f.chunk.js"
+  },
+  {
+    "revision": "04704aa443343c248d8b",
+    "url": "/Skim/WEB215/Lesson14/static/js/runtime-main.54e6527a.js"
+  },
+  {
+    "revision": "87b29f699489dcb1c09b472358d91e46",
+    "url": "/Skim/WEB215/Lesson14/static/media/Sona.87b29f69.jpg"
+  }
+]);
